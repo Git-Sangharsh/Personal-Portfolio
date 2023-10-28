cc9fc5d4b1505f7498e64dfb235f07c91205ca6d
Hitesh Ram <hiteshram321@gmail.com>
2023-10-28 22:27:39 +0530

    Email added

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,7 @@
+import { useRef, useState } from "react";
 import "./contact.scss";
 import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
 
 const variants = {
   initial: {
@@ -17,6 +19,30 @@ const variants = {
 };
 
 const Contact = () => {
+  const formRef = useRef();
+  const [error, setError] = useState(false);
+  const [success , setSuccess] = useState(false)
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_x68cjug",
+        "template_ikdx0fi",
+        formRef.current,
+        "XraAWQNA_p3Ir57jL"
+      )
+      .then(
+        (result) => {
+          setSuccess(true);
+        },
+        (error) => {
+          setError(true);
+        }
+      );
+  };
+
   return (
     <motion.div
       className="contact"
@@ -36,16 +62,19 @@ const Contact = () => {
         </motion.div>
       </motion.div>
       <div className="formContainer">
-        
         <motion.form
+          ref={formRef}
+          onSubmit={sendEmail}
           initial={{ opacity: 0 }}
           whileInView={{ opacity: 1 }}
           transition={{ delay: 1, duration: 1 }}
         >
-          <input type="text" required placeholder="Name" />
-          <input type="email" required placeholder="Email" />
-          <textarea rows={8} placeholder="message" />
+          <input type="text" required placeholder="Name" name="name"/>
+          <input type="email" required placeholder="Email" name="email"/>
+          <textarea rows={8} placeholder="message" name="message"/>
           <button>Submit</button>
+          {error && 'Error'}
+          {success && 'Success'}
         </motion.form>
       </div>
     </motion.div>
